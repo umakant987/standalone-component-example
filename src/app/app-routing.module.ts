fbc200fcb813a4519ec9081c49906e67878aa4a3
Umakant Lakhawar <umakant.lakhawar@gmail.com>
2023-07-25 20:40:36 +0530

    done changes for walk my dog

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DogsListComponent } from './dogs-list.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'list' },
+  { path: 'list', component: DogsListComponent },
+  { path: 'details/:index', loadComponent: () => import('./dog-view.component').then(m => m.DogViewComponent) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
